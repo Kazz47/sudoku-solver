a7d80e6c6be18dd027f5eae07285ae84e98449ed
Kyle Goehner <KyleA.Goehner@gmail.com>
2014-06-24 21:05:36 -0400

    Added code to solve a sudoku puzzle.

--- main.py
+++ main.py
@@ -4,69 +4,11 @@ import boards
 import solver
 
 def check_sudoku(grid):
-    # Sanity Check
-    if(len(grid) != 9):
-        return None
+    if (solver.validGrid(grid)):
+        if (solver.solveSudoku(grid)):
+            return solver.validGrid(grid)
     else:
-        for row in grid:
-            if(len(row) != 9):
-                return None
-            else:
-                for item in row:
-                    if(not isinstance(item, int) or item < 0 or item > 9):
-                        return None
-
-    # Check correct number of vals in each row, column, and box (skip when we hit a zero)
-
-    found_zero = False
-
-    # Check rows
-    for row in range(9):
-        bit_list = [False] * 10;
-        for col in range(9):
-            current_item = grid[row][col]
-            if(current_item != 0 and bit_list[current_item]):
-                print("{0} was duplicated in row {1}".format(current_item, row))
-                return False
-            else:
-                bit_list[current_item] = True
-
-    # Check columns
-    for col in range(9):
-        bit_list = [False] * 10;
-        for row in range(len(grid)):
-            current_item = grid[row][col]
-            if(current_item != 0 and bit_list[current_item]):
-                print("{0} was duplicated in column {1}".format(current_item, row))
-                return False
-            else:
-                bit_list[current_item] = True
-
-    # Check boxes
-    start_positions = [[0,0],
-                       [0,3],
-                       [0,6],
-                       [3,0],
-                       [3,3],
-                       [3,6],
-                       [6,0],
-                       [6,3],
-                       [6,6]]
-    for i in range(9):
-        x = start_positions[i][0]
-        y = start_positions[i][1]
-        bit_list = [False] * 10;
-        for row in range(3):
-            for col in range(3):
-                current_item = grid[y+row][x+col]
-                if(current_item != 0 and bit_list[current_item]):
-                    print("{0} was duplicated in box ({1},{2})".format(current_item, x//3, y//3))
-                    return False
-                else:
-                    bit_list[current_item] = True
-
-    return solver.solveSudoku(grid)
-
+        return False
 
 print(check_sudoku(boards.ill_formed))
 print(check_sudoku(boards.ill_formed_2))
